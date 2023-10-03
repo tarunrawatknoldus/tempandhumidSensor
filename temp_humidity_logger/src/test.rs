bb4bf839efa9d716bcf81db8fe2b586997be16fa
Tarun Singh Rawat <126674031+tarunrawatknoldus@users.noreply.github.com>
2023-10-03 12:57:58 +0530

    Update test.rs

--- temp_humidity_logger/src/test.rs
+++ temp_humidity_logger/src/test.rs
@@ -2,25 +2,18 @@ use simple_dht11::dht11::Dht11;
 use std::thread::sleep;
 use std::time::Duration;
 use std::error::Error;
-use csv::Writer;
-use csv::WriterBuilder;
 use std::fs::OpenOptions;
+use std::io::Write;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut dht11 = Dht11::new(27); // Note this is BCM
 
     // Open or create the CSV file for appending data
-    let file = OpenOptions::new()
+    let mut file = OpenOptions::new()
         .create(true)
         .write(true)
         .append(true)
         .open("sensor_data.csv")?;
-
-    // Create a CSV writer with custom delimiter and headers
-    let mut csv_writer = WriterBuilder::new()
-        .delimiter(b',')
-        .has_headers(true)
-        .from_writer(file);
 
     loop {
         let response = dht11.get_reading();
@@ -28,8 +21,18 @@ fn main() -> Result<(), Box<dyn Error>> {
         println!("Temperature: {}°C", response.temperature);
         println!("Humidity: {}%", response.humidity);
 
+        // Format the data as a CSV line
+        let csv_line = format!("{}, {}\n", response.temperature, response.humidity);
+
         // Write data to the CSV file
-        csv_writer.serialize((response.temperature, response.humidity))?;
+        if let Err(err) = file.write_all(csv_line.as_bytes()) {
+            eprintln!("Error writing data to CSV: {}", err);
+        }
+
+        // Flush the file to ensure data is written immediately
+        if let Err(err) = file.flush() {
+            eprintln!("Error flushing data to CSV: {}", err);
+        }
 
         // Sleep for 1 second before the next reading
         sleep(Duration::from_secs(1));
