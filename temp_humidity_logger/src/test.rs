d3f4a5c50d6d7e01b66adbd28c920508152497b1
Tarun Singh Rawat <126674031+tarunrawatknoldus@users.noreply.github.com>
2023-10-03 11:40:05 +0530

    Update test.rs

--- temp_humidity_logger/src/test.rs
+++ temp_humidity_logger/src/test.rs
@@ -1,25 +1,77 @@
-use simple_dht11::dht11::Dht11;
+use rppal::gpio::Gpio;
+use rppal::system::DeviceInfo;
 use std::fs::OpenOptions;
 use std::io::{self, Write};
+use std::thread::sleep;
+use std::time::Duration;
 
 fn main() {
-    // Create a new Dht11 instance, specifying the GPIO pin where the sensor is connected (BCM numbering).
-    let mut dht11 = Dht11::new(27); // Note this is BCM
+    // Get the BCM pin number for the GPIO pin connected to the DHT11 sensor.
+    let pin_num = 27;
 
-    // Use the Dht11 instance to get sensor reading data.
-    let response = dht11.get_reading();
+    // Create a GPIO instance.
+    let gpio = Gpio::new().expect("Failed to create GPIO instance");
+
+    // Get the pin associated with the BCM pin number.
+    let pin = gpio
+        .get(pin_num)
+        .expect("Failed to get pin")
+        .into_input_pullup();
+
+    // Read sensor data.
+    let (temperature, humidity) = read_dht11(&pin).expect("Failed to read DHT11 sensor");
 
     // Print the temperature and humidity readings to the console.
-    println!("Temperature: {}°C", response.temperature);
-    println!("Humidity: {}%", response.humidity);
+    println!("Temperature: {}°C", temperature);
+    println!("Humidity: {}%", humidity);
 
     // Store the data in a local file.
-    if let Err(err) = save_to_file(response) {
+    if let Err(err) = save_to_file(temperature, humidity) {
         eprintln!("Error: {}", err);
     }
 }
 
-fn save_to_file(response: simple_dht11::dht11::Reading) -> io::Result<()> {
+fn read_dht11(pin: &rppal::gpio::InputPin) -> Result<(f32, f32), rppal::gpio::Error> {
+    // Send start signal by pulling the pin low for at least 18ms.
+    pin.set_mode(rppal::gpio::Mode::Output)?;
+    pin.set_low();
+    sleep(Duration::from_millis(18));
+
+    // Switch the pin to input mode and wait for the sensor response.
+    pin.set_mode(rppal::gpio::Mode::Input)?;
+    sleep(Duration::from_micros(40));
+
+    // Read the response from the sensor.
+    let mut data = [0u8; 5];
+    for i in 0..40 {
+        sleep(Duration::from_micros(1));
+        if pin.is_high() {
+            let index = i / 8;
+            data[index] <<= 1;
+            data[index] |= 1;
+        } else {
+            let index = i / 8;
+            data[index] <<= 1;
+        }
+    }
+
+    // Verify the checksum of the received data.
+    let checksum = (data[0] as u16)
+        .wrapping_add(data[1] as u16)
+        .wrapping_add(data[2] as u16)
+        .wrapping_add(data[3] as u16);
+    if checksum != (data[4] as u16) {
+        return Err(rppal::gpio::Error::Unknown);
+    }
+
+    // Parse the temperature and humidity values from the received data.
+    let temperature = (data[2] as f32) + (data[3] as f32) / 10.0;
+    let humidity = (data[0] as f32) + (data[1] as f32) / 10.0;
+
+    Ok((temperature, humidity))
+}
+
+fn save_to_file(temperature: f32, humidity: f32) -> io::Result<()> {
     // Open or create a file named "sensor_data.txt" for appending.
     let mut file = OpenOptions::new()
         .create(true)
@@ -28,7 +80,7 @@ fn save_to_file(response: simple_dht11::dht11::Reading) -> io::Result<()> {
         .open("sensor_data.txt")?;
 
     // Format the data as a string and write it to the file.
-    let data = format!("Temperature: {}°C, Humidity: {}%\n", response.temperature, response.humidity);
+    let data = format!("Temperature: {}°C, Humidity: {}%\n", temperature, humidity);
     file.write_all(data.as_bytes())?;
 
     Ok(())
